6a3f681c602390ea92ccbca1905dcd6fd6a0d08d
Yevhenii <polotnyak2004@gmail.com>
2024-06-06 17:19:24 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+[
+    ...document.querySelectorAll("li")
+].forEach((li)=>{
+    const span = document.createElement("span");
+    span.textContent = li.firstChild.textContent;
+    li.firstChild.textContent = "";
+    li.prepend(span);
+});
+document.querySelector(".tree").addEventListener("click", (e)=>{
+    const li = e.target.closest("li");
+    if (li.children.length > 1 && e.target === li.firstChild) {
+        if (li.lastElementChild.style.display === "none") li.lastElementChild.style.display = "block";
+        else li.lastElementChild.style.display = "none";
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
